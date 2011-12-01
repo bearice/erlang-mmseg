8d49b64753f3afafe2ece4dc2dc383af01f41e77
Bearice Ren <bearice@gmail.com>
2011-12-01 10:51:35 +0800

    using spawn on ngram

--- src/ngram.erl
+++ src/ngram.erl
@@ -10,6 +10,23 @@ break_word([12377,12391|_]) -> %desu
 break_word(_)->
     0.
 
+english([Ch|Tail],Acc,Handler) when (($a =< Ch) and (Ch =< $z)) or (($A =< Ch) and (Ch =< $Z)) or (($0 =< Ch) and (Ch =< $9)) or (Ch == $-) or (Ch == $_) ->
+    english(Tail,[Ch|Acc],Handler);
+
+english([_|Tail],[],Handler) ->
+    english(Tail,[],Handler);
+
+english([_|Tail],Acc,Handler) ->
+    Handler(list_to_tuple(lists:reverse(Acc))),
+    english(Tail,[],Handler);
+
+english([],Acc,Handler) ->
+    case Acc of
+        [] -> ok;
+        _  -> 
+            Handler(list_to_tuple(lists:reverse(Acc))),
+            ok
+    end.
 
 unigram([$\n|Tail],Handler) ->
     unigram(Tail,Handler);
@@ -87,38 +104,44 @@ fivegram([H1,H2,H3,H4,H5|Tail],Handler) ->
 
 fivegram(_,_) -> ok.
 
-%ngram(1,String,Handler) -> unigram(String,Handler);
-%ngram(2,String,Handler) -> bigram(String,Handler);
-%ngram(3,String,Handler) -> trigram(String,Handler);
-%ngram(4,String,Handler) -> fourgram(String,Handler);
-%ngram(5,String,Handler) -> fivegram(String,Handler);
+ngram(1,String,Handler) -> unigram(String,Handler);
+ngram(2,String,Handler) -> bigram(String,Handler);
+ngram(3,String,Handler) -> trigram(String,Handler);
+ngram(4,String,Handler) -> fourgram(String,Handler);
+ngram(5,String,Handler) -> fivegram(String,Handler);
 
 ngram(N,String,Handler) when length(String) > N ->
     lists:foldl(fun ngram_fold/2,{N,Handler,0,[]},String).
+
+ngram_fold(X,{N,H,N,Acc}) ->
+        String = lists:reverse(Acc),
+        Token = list_to_tuple(String),
+        H(Token),
+        [_|Tail] = String,
+        NewString = lists:reverse(Tail),
+        ngram_fold(X,{N,H,N-1,NewString});
 
 ngram_fold($\n,{N,H,_,_}) ->
         {N,H,0,[]};
 
 ngram_fold(X,{N,H,L,Acc}) when L<N ->
-        {N,H,L+1,[X|Acc]};
+        {N,H,L+1,[X|Acc]}.
 
-ngram_fold(X,{N,H,N,Acc}) ->
-        Token = list_to_tuple(lists:reverse(Acc)),
-        H(Token),
-        {N,H,1,[X]}.
 
-filter(16#76ee,_)       -> {16#76ee,16#7684} ;   % 目的
-filter(16#7684,16#7684) -> {16#7684,0} ;         % 的
-filter(16#7684,_)       -> {$\n,0} ;             % 的
+filter_english(Char,_) when (Char == $-) or (Char == $_) -> {Char,0};
+filter_english(Char,_) when ($a =< Char) and (Char =< $z) -> {Char,0};
+filter_english(Char,_) when ($A =< Char) and (Char =< $Z) -> {Char,0};
+filter_english(Char,_) when ($0 =< Char) and (Char =< $9) -> {Char,0};
+filter_english(_,_) -> {$\n,0}.
 
-filter(Char,_) when ($a =< Char) and (Char =< $z) -> {Char,0};
-filter(Char,_) when ($A =< Char) and (Char =< $Z) -> {Char,0};
-filter(Char,_) when ($0 =< Char) and (Char =< $9) -> {Char,0};
-filter(Char,_) when (16#4e00 =< Char) and (Char =< 16#9fff) -> {Char,0};
-filter(Char,_) when (16#3400 =< Char) and (Char =< 16#4dbf) -> {Char,0};
-filter(Char,_) when (16#3041 =< Char) and (Char =< 16#309f) -> {Char,0};
-filter(Char,_) when (16#30a0 =< Char) and (Char =< 16#30ff) -> {Char,0};
-filter(_,_) -> {$\n,0}.
+filter_cjk(16#76ee,_)       -> {16#76ee,16#7684} ;   % 目的
+filter_cjk(16#7684,16#7684) -> {16#7684,0} ;         % 的
+filter_cjk(16#7684,_)       -> {$\n,0} ;             % 的
+filter_cjk(Char,_) when (16#4e00 =< Char) and (Char =< 16#9fff) -> {Char,0};
+filter_cjk(Char,_) when (16#3400 =< Char) and (Char =< 16#4dbf) -> {Char,0};
+filter_cjk(Char,_) when (16#3041 =< Char) and (Char =< 16#309f) -> {Char,0};
+filter_cjk(Char,_) when (16#30a0 =< Char) and (Char =< 16#30ff) -> {Char,0};
+filter_cjk(_,_) -> {$\n,0}.
 
 
 handle_word(String) ->
@@ -145,20 +168,39 @@ test2() ->
     end,R),
     ok.
 
+do_parallel(Jobs) ->
+    Parent = self(),
+    Ref = make_ref(),
+    [receive {Ref, Pid, Result} -> Result end
+        || Pid <- [spawn_link(fun() -> Parent ! {Ref, self(), X()} end) || X <- Jobs]].
+
 test() ->
-    {ok,Data} = file:read_file("in.txt"),
-    S = unicode:characters_to_list(Data),
-    {SS,_} = lists:mapfoldl(fun filter/2,0,S),
-    %io:format("~ts~n",[SS]),
     {ok,P} = wordfeq:start([]),
     F = fun(W) -> wordfeq:handle_word(P,W) end,
+
+    {ok,Data} = file:read_file("in.txt"),
+    S = unicode:characters_to_list(Data),
+
+    T0 = now(),
+    [SS,SE] = do_parallel([
+         fun() -> {SS,_} = lists:mapfoldl(fun filter_cjk/2,0,S),    SS end
+        ,fun() -> {SE,_} = lists:mapfoldl(fun filter_english/2,0,S),SE end
+    ]),
+
+    %io:format("~ts~n",[SS]),
     T1 = now(),
-    %ngram(1,SS,F),
-    ngram(2,SS,F),
-    ngram(3,SS,F),
-    ngram(4,SS,F),
-    ngram(5,SS,F),
-    ngram(6,SS,F),
+    TDiff0 = timer:now_diff(T1,T0),
+    io:format("Filter Time used: ~p~n",[TDiff0]),
+
+    do_parallel([
+         fun() -> english(SE,[],F) end
+%       ,fun() -> ngram(1,SS,F) end
+        ,fun() -> ngram(2,SS,F) end
+        ,fun() -> ngram(3,SS,F) end
+        ,fun() -> ngram(4,SS,F) end
+        ,fun() -> ngram(5,SS,F) end
+%       ,fun() -> ngram(6,SS,F) end
+    ]),
     
     T2 = now(),
     TDiff1 = timer:now_diff(T2,T1),
